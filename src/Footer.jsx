bfd2f412211e574187d40cc8fa3fc71be43b9f48
Shubham Vishwakarma <vi453770@gmail.com>
2024-08-22 08:41:09 +0530

    introduction or basic are complete of react component

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()}Our Web Site </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Wel-Come To MyWebSite</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About US</a>
+            </li>
+            <li>
+              <a href="#">Contact US</a>
+            </li>
+            <li>
+              <a href="#">Our Work</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
